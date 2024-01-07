d0029db89d3423229f9c94c836513ab77de1c73a
Max Atslega <max@atslega.dev>
2024-01-07 16:02:22 +0100

    Add: Cross build for raspberry pi

---
+++ crossbuild.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+set -o errexit
+set -o nounset
+set -o pipefail
+set -o xtrace
+
+readonly TARGET_ARCH=aarch64-unknown-linux-gnu
+readonly LINK_FLAGS='-L /usr/arm-linux-gnueabihf/lib/ -L /usr/lib/arm-linux-gnueabihf/'
+
+RUSTFLAGS=${LINK_FLAGS} cross build --release --target=${TARGET_ARCH}
+
