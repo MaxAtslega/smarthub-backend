ef407309722e687b967d7a74210bc54a6c89bea5
Max Atslega <max@atslega.dev>
2024-01-24 20:40:24 +0100

    Fix: Permission in target

--- crossbuild.sh
+++ crossbuild.sh
@@ -7,5 +7,7 @@ set -o xtrace
 readonly TARGET_ARCH=aarch64-unknown-linux-gnu
 readonly LINK_FLAGS='-L /usr/arm-linux-gnueabihf/lib/ -L /usr/lib/arm-linux-gnueabihf/'
 
+chmod -R 777 ./target/
+
 RUSTFLAGS=${LINK_FLAGS} cross build --release --target=${TARGET_ARCH}
 
